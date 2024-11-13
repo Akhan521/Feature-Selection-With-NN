d53b74793bb9a06ce009555b23295af348992bde
Aamir Khan <aamirksfg@gmail.com>
2024-11-12 21:47:41 -0800

    initializing main

--- main.py
+++ main.py
@@ -5,11 +5,22 @@ from util import *
 from forward_selection import forward_selection
 from backward_elimination import backward_elimination
 
+print('\nWelcome to Aamir Khan\'s Feature Selection Algorithm:')
+print('-----------------------------------------------------\n')
+
+
 # Setting the number of features we're considering.
-num_features = 4
+num_features = int(input('Enter the number of features you\'d like to consider: '))
 
-# Running the forward selection algorithm.
-#max_score, best_subset = forward_selection(num_features)
+print('''\nSelect the algorithm you\'d like to run:\n
+      1. Forward Selection\n
+      2. Backward Elimination
+      ''')
+choice = input('Enter your choice: ')
 
-# Running the backward elimination algorithm.
-max_score, best_subset = backward_elimination(num_features)
+if choice == '1':
+    # Running the forward selection algorithm.
+    max_score, best_subset = forward_selection(num_features)
+else:
+    # Running the backward elimination algorithm.
+    max_score, best_subset = backward_elimination(num_features)
